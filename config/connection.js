56450662838debb450ecf035aaeff5968c7c26ac
unknown <pratttrevor79@gmail.com>
2022-08-07 21:09:34 -0500

    updating routes...so many routes

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+//Import sequelize constructor
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW , {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3006
+});
+
+
+module.exports = sequelize;
