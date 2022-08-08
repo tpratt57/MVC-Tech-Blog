e12224faaefb61623c14be702d5d59fde17affc7
unknown <pratttrevor79@gmail.com>
2022-08-07 23:11:40 -0500

    idk what was changed, so sleepy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./controllers');
-const sequelzie = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const session = require('express-session');
+const sequelzie = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,13 +26,14 @@ const sess = {
 
 app.use(session(sess));
 const hbs = exphbs.create({helpers});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, "views"));
 
 app.use(routes);
 
